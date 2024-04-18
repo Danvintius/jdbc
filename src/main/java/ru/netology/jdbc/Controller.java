4cdbe9614b991ffed006aef753879453c4af02b9
Azerty <danila.94@mail.ru>
2024-04-18 20:12:49 +0300

    2

--- src/main/java/ru/netology/jdbc/Controller.java
+++ src/main/java/ru/netology/jdbc/Controller.java
@@ -1,4 +1,17 @@
 package ru.netology.jdbc;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collections;
+import java.util.List;
+
+@org.springframework.stereotype.Controller
 public class Controller {
+    Repository repository = new Repository();
+
+    @GetMapping("/products/fetch-product")
+    public List<String> getProductName(@RequestParam(value = "name", required = false) String name) {
+        return repository.getProductName(name);
+    }
 }
--- src/main/java/ru/netology/jdbc/Repository.java
+++ src/main/java/ru/netology/jdbc/Repository.java
@@ -1,17 +1,44 @@
 package ru.netology.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import javax.sql.DataSource;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @org.springframework.stereotype.Repository
 public class Repository {
-    public static void main(String[] args) {
+    @Autowired
+    private static DataSource dataSource;
 
+    @Autowired
+    private static JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private static String script = read("data.sql");
+
+    public static void main(String[] args) throws SQLException {
+        Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement();
+
+        List<Product> products = jdbcTemplate.query(script, (resultSet, rowNum) -> {
+            String name = resultSet.getString("name");
+            return new Product(name);
+
+        });
+        products.forEach(System.out::println);
     }
 
     public static String read(String scriptFileName) {
@@ -23,7 +50,7 @@ public class Repository {
         }
     }
 
-    private String getProductName(String name) {
-        return name;
+    List<String> getProductName(String name) {
+        return namedParameterJdbcTemplate.queryForList(script, Map.of("name", name), String.class);
     }
 }
